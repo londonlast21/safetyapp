d91bab7842c01ac7ecc4fe8a18da767dc3791aa2
jack <biffisbuffm@gmail.com>
2020-11-07 17:42:29 -0600

    push

--- server/server.js
+++ server/server.js
@@ -6,10 +6,10 @@ const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
 const db = require('./config/connection');
 
+const app = express();
 
 
 const PORT = process.env.PORT || 3001;
-const app = express();
 
 // create the apollo server here
 const server = new ApolloServer({
