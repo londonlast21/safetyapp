ce638d0158876ac7d9962acd4c6f729bfc2020f0
jack <biffisbuffm@gmail.com>
2020-11-07 16:48:41 -0600

    eduit

--- server/server.js
+++ server/server.js
@@ -25,11 +25,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path_join(__dirname, '../client/build/index.html')));
+  app.use(express.static(path.join(__dirname, '../client/build/index.html')));
 }
 
 app.get('*', (req, res) => {
-  res.sendFile(path_join(__dirname, '../client/build/index.html'));
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
 })
 
 db.once('open', () => {
