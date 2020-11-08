ef79e8447623f9356c0034be7cf7a571a0b541ef
jack <biffisbuffm@gmail.com>
2020-11-08 11:48:39 -0600

    pwa

--- client/public/serviceWorker.js
+++ client/public/serviceWorker.js
@@ -32,7 +32,7 @@ const isLocalhost = Boolean(
       }
   
       window.addEventListener('load', () => {
-        const swUrl = `${process.env.PUBLIC_URL}/serviceWorker.js`;
+        const swUrl = `${process.env.PUBLIC_URL}/client/public/serviceWorker.js`;
   
         if (isLocalhost) {
           // This is running on localhost. Let's check if a service worker still exists or not.
