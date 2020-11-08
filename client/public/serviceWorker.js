f4d402bbe99f45bd0ef7cd4b1e8b0071290f4f93
jack <biffisbuffm@gmail.com>
2020-11-08 16:57:43 -0600

    heroku

--- client/public/serviceWorker.js
+++ client/public/serviceWorker.js
@@ -10,20 +10,20 @@ const FILES_TO_CACHE = [
   "/icons/logo192-192x192.png",
   "/icons/logo512-512x512.png",
 
-  // "/src/components/DeleteButton.js",
+  "/src/components/DeleteButton.js",
   "/src/components/Navbar.js",
-  // "/src/components/PostCard.js",
-  // "/src/components/PostForm.js",
+  "/src/components/PostCard.js",
+  "/src/components/PostForm.js",
 
   "/src/pages/Home.js",
-  // "/src/pages/Login.js",
-  // "/src/pages/Signup.js",
+  "/src/pages/Login.js",
+  "/src/pages/Signup.js",
   
 
-  // // "./src/utils/auth.js",
-  // // "./src/utils/hooks.js",
-  // // "./src/utils/queries.js",
-  // // "./src/utils/mutations.js",
+  "/src/utils/auth.js",
+  "/src/utils/hooks.js",
+  "/src/utils/queries.js",
+  "/src/utils/mutations.js",
 
   "/src/App.js",
   "/src/App.css",
