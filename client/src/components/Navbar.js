53b4fcf624ebcce6105ad51286f9fb64ef61ee42
jack <biffisbuffm@gmail.com>
2020-11-07 18:32:40 -0600

    edit

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -34,7 +34,7 @@ const Navbar= () => {
     // menu from user who is logged in
     <Menu pointing size="huge" color="grey">
       <Menu.Item
-        name={loggedIn.name}
+        name={loggedIn}
         active
         as={Link}
         to="/"
