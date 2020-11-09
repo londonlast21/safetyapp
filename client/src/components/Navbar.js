d824ee13e2f2bfa238671279a39a242e4b287932
jack <biffisbuffm@gmail.com>
2020-11-08 23:00:34 -0600

    edit

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -20,9 +20,10 @@ const Navbar= () => {
   // get username from logged in user??
   // const username = ;
   const { data: userData } = useQuery(QUERY_ME);
-  const user = {userData.me.username};
+  const user = {userData };
 
-  console.log(user);
+  const name = userData?.username;
+  console.log(name);
 
 
 
